45f5bc2b9158c323120a67027a960aa20a8256d3
code-combo <rjayjutolu419@gmail.com>
2023-11-22 14:18:14 +0100

    Setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 
+// Styles
 import './App.css'
+
+// Page components
+import RootLayout from "./layouts/RootLayout"
+import Notes from "../src/pages/notes/Notes"
+import Reminders from "../src/pages/reminders/Reminders"
+import Archive from "../src/pages/archive/Archive"
+import Trash from "../src/pages/trash/Trash"
 
 function App() {
 
-  const router = createBrowser
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path = "/" element = {<RootLayout/>}>
+        <Route index element = {<Notes/>}/>
+        <Route path = "reminders" element = {<Reminders/>}/>
+        <Route path = "archive" element = {<Archive/>}/>
+        <Route path = "trash" element = {<Trash/>}/>
+      </Route>
+    )
+  )
 
   return (
-    <>
-    </>
+    <RouterProvider router = {router}/>
   )
 }
 
